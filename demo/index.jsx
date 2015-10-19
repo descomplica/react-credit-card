73475a6ec40df685cdaf8b4c60d5b2347e5d7bc1
Giovanni keppelen <keppelen.as@gmail.com>
2015-10-19 10:24:13 -0200

    better demo view

--- demo/index.jsx
+++ demo/index.jsx
@@ -2,28 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import CreditCard from '../src/creditCard';
 
-class Demo extends React.Component {
-
-  render() {
-    return (
-      <CreditCard success={function(data){console.log(data)}} />
-    );
-  }
-
-}
-
 ReactDOM.render(
-  <Demo />,
+  <CreditCard success={function(data){console.log(data)}} />,
   document.getElementById('demo')
 );
 ReactDOM.render(
-  <Demo />,
+  <CreditCard installments={3} price={1300} />,
   document.getElementById('demo1')
 );
 ReactDOM.render(
-  <Demo />,
+  <CreditCard />,
   document.getElementById('demo2')
 );
